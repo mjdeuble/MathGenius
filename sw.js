41573d88886db136a937c028b8a2af35e5c1ab58
mjdeuble <mjdeuble@gmail.com>
2025-10-18 08:22:24 +1000

    Add service worker for caching static assets
    Implement service worker for caching assets and offline support.

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'times-tables-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  // Make sure to include your icons!
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png' 
+];
+
+// Install event: Caches all static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: Serves cached assets when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event: Clears old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
